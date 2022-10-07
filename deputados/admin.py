389ea91086b1b2d7f0d4e9cbcade2dbd0539bf8a
Felipe Colen <felipecolen@gmail.com>
2022-10-07 17:45:43 -0400

    feat: adicionado id na listagem no admin

--- deputados/admin.py
+++ deputados/admin.py
@@ -5,6 +5,6 @@ from deputados.models import Deputado
 
 @admin.register(Deputado)
 class DeputadoAdmin(admin.ModelAdmin):
-    list_display = ('id_api', 'nome', 'sigla_partido', 'sigla_uf')
-    list_filter = ('sigla_partido', 'sigla_uf')
-    search_fields = ('nome', )
+    list_display = ("id", "id_api", "nome", "sigla_partido", "sigla_uf")
+    list_filter = ("sigla_partido", "sigla_uf")
+    search_fields = ("nome",)
